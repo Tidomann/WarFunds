7c61d82548514ce87bcfbab6c1eb34919b07da9c
Tidomann <tidomann@gmail.com>
2021-09-25 20:35:37 -0400

    I added a comment
    A Comment was added to the godot script file to show how changes are shown on the branches

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -4,7 +4,7 @@ extends Control
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+# this an example comment
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
