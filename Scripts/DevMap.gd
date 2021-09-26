28f40b2fe946b7303f78bb70adf9f6d92a1ed28f
Tidomann <tidomann@gmail.com>
2021-09-25 20:51:02 -0400

    Test this Branch
    Testing Branch Github

--- Scripts/DevMap.gd
+++ Scripts/DevMap.gd
@@ -4,7 +4,7 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+# this is a useless comment
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
