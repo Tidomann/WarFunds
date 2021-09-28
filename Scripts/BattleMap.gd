31ec22435efc9f1d81ef5f11475c331ad84bf567
Tidomann <tidomann@gmail.com>
2021-09-28 02:22:43 -0400

    Added Cursor
    Added a cursor that kind of cheats to show onto the appropriate grid that is being highlighted.

--- Scripts/BattleMap.gd
+++ Scripts/BattleMap.gd
@@ -4,10 +4,13 @@ extends Node2D
 # var a = 2
 # var b = "text"
 # this is a useless comment
+var cursor = preload("res://Scenes/Cursor.tscn")
+var cursorSprite
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	setup_tiles()
+	setup_cursor()
 	
 
 func setup_tiles():
@@ -16,11 +19,16 @@ func setup_tiles():
 		var tileIndex = $RenderedTiles.tile_set.find_tile_by_name(
 			$Devtiles.tile_set.tile_get_name($Devtiles.get_cellv(cell)))
 		$RenderedTiles.set_cellv(cell, tileIndex)
-		if tileIndex == Constants.TILE.SEA || tileIndex == Constants.TILE.ROAD || tileIndex == Constants.TILE.SHOAL:
+		if tileIndex == Constants.TILE.SEA || tileIndex == Constants.TILE.ROAD:
 			$RenderedTiles.update_bitmask_area(cell)
 			
+
+func setup_cursor():
+	cursorSprite = cursor.instance()
+	add_child((cursorSprite))
 		
-		
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	cursorSprite.position = get_global_mouse_position().snapped(Vector2(16,16))
+	#print($Devtiles.world_to_map((get_global_mouse_position())))
